60dfd62f2646a8a4dc6164bf3b6f2ee0abee49b7
Mathis Randl <mathis.randl@epfl.ch>
2023-08-17 10:31:50 +0200

    made buttons aligned

--- lib/dbInteractRoute.dart
+++ lib/dbInteractRoute.dart
@@ -54,54 +54,64 @@ class _DbInteractRouteState extends State<DbInteractRoute> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
 
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _counter++;
+                      _dogsList = _runInsert(_counter, "doggo $_counter")
+                          .then((v) => _getRefreshedList());
+                    });
+                  },
+                  child: const Text('Insert'),
+                ),
+                const SizedBox(width: 10,),
+                // clean the database and update the visuals
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _dogsList = _runWipeAll().then((v) => _getRefreshedList());
+                    });
+                  },
+                  child: const Text('Wipe database'),
+                ),
+              ],
+            ),
             // insert new dog into db
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  _counter++;
-                  _dogsList = _runInsert(_counter, "doggo $_counter")
-                      .then((v) => _getRefreshedList());
-                });
-              },
-              child: const Text('Insert'),
-            ),
+
             const SizedBox(height: 20,),
 
-            // clean the database and update the visuals
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  _dogsList = _runWipeAll().then((v) => _getRefreshedList());
-                });
-              },
-              child: const Text('Wipe database'),
+            Expanded(
+              child: FutureBuilder<List<Dog>>(
+                future: _dogsList,
+                initialData: const <Dog>[],
+                builder: (context, snapshot) {
+                  final dogDump = snapshot.data != null ? snapshot.data! : <Dog>[];
+                  return Card(child : ClipRRect(
+                    borderRadius: BorderRadius.circular(12.0),
+                    child : Container(
+                      constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.85),
+                      child: Scrollbar(
+                        child: ListView.builder(
+                          scrollDirection: Axis.vertical,
+                          itemCount: dogDump.length,
+                          itemBuilder: (context, index) {
+                            final item = dogDump[index];
+                            return ListTile(
+                              title: Text(item.name),
+                              subtitle: Text(item.id.toString()),
+                            );
+                          },
+                        )
+                      )
+                    )
+                  ));
+                },
+              )
             ),
             const SizedBox(height: 20,),
-
-            FutureBuilder<List<Dog>>(
-              future: _dogsList,
-              initialData: const <Dog>[],
-              builder: (context, snapshot) {
-                final dogDump = snapshot.data != null ? snapshot.data! : <Dog>[];
-                return Card(child : ClipRRect(
-                  borderRadius: BorderRadius.circular(12.0),
-                  child : Container(
-                    constraints: BoxConstraints(minHeight: 400, maxHeight: 400, maxWidth: MediaQuery.of(context).size.width * 0.85),
-                    child: Scrollbar(child: ListView.builder(
-                      scrollDirection: Axis.vertical,
-                      itemCount: dogDump.length,
-                      itemBuilder: (context, index) {
-                        final item = dogDump[index];
-                        return ListTile(
-                          title: Text(item.name),
-                          subtitle: Text(item.id.toString()),
-                        );
-                      },
-                    ))
-                  )
-                ));
-              },
-            ),
           ]
         )
       ),
